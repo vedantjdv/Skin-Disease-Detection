64897d8a33b4005febb5df777f5c6f0a18c3d33a
Aakash Patwardhan <66508484+Aakash2201@users.noreply.github.com>
2022-10-14 13:54:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,45 @@ image_input.addEventListener("change", function() {
   reader.addEventListener("load", () => {
     const uploaded_image = reader.result;
     document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+    // var imgFullURL = uploaded_image.src;
+    // console.log(imgFullURL);
+    // if(imgFullURL=="947882.jpg")
+    // {
+    //   document.querySelector(".aakash").style.display = "block";
+    // }
   });
   reader.readAsDataURL(this.files[0]);
+
 });
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+}
